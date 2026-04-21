16684d0b99ab11dccc74a1cd19ede6bae74f206f
abelucci <abelash.delsur@gmail.com>
2026-04-21 14:05:42 -0300

    change code

--- app/calculator.py
+++ app/calculator.py
@@ -3,3 +3,23 @@ def add(a, b):
 
 def divide (a, b):
     return a / b
+
+# Entrada de datos
+print("📌 Calculadora simple")
+print("1 - Sumar")
+print("2 - Dividir")
+
+opcion = input("Elegí una opción: ")
+
+num1 = float(input("Ingresá el primer número: "))
+num2 = float(input("Ingresá el segundo número: "))
+
+if opcion == "1":
+    print(f"Resultado: {add(num1, num2)}")
+elif opcion == "2":
+    try:
+        print(f"Resultado: {divide(num1, num2)}")
+    except ZeroDivisionError:
+        print("❌ No se puede dividir por cero")
+else:
+    print("❌ Opción inválida")
--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@
 Instalar dependencias:
 
 ```
-pip install **-r** requirements.txt
+pip install -r requirements.txt
 ```
 
 Ejecutar reporte:
